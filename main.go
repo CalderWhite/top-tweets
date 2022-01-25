4d134f23781be8308a73f85c47406c21912617f2
Calder White <calderwhite1@gmail.com>
2022-01-24 22:54:59 -0500

    Reworked package structure.

---
+++ main.go
@@ -0,0 +1,203 @@
+package main
+
+import (
+	"strconv"
+    "io"
+    "time"
+    "log"
+
+	"github.com/gin-gonic/gin"
+    "github.com/CalderWhite/top-tweets/lib"
+)
+
+/**
+ * NOTE: Caching should be considered for all endpoints, since they all run their respective queries fully.
+ */
+
+func main() {
+	r := gin.Default()
+	r.Static("/static", "./build/static")
+
+    api := r.Group("/api")
+
+	/**
+	 * Gets the top [limit] words (default 100), adjusted by the longGlobalDiff.
+	 * This adjustment allows top to produce emerging and interesting words, instead of
+	 * stopwords like "the" or "los" (in spanish), etc.
+	 */
+	api.GET("/words/top", func(c *gin.Context) {
+		q := c.Request.URL.Query()
+		limitParam, found := q["limit"]
+		var limit int
+		// a limit of 100 by default
+		if !found {
+			limit = 100
+		} else {
+			var err error
+			limit, err = strconv.Atoi(limitParam[0])
+			if err != nil {
+				c.JSON(400, gin.H{
+					"message": "Error! Limit query param must be integer.",
+				})
+				return
+			}
+		}
+
+		words := getTop(limit)
+		// reverse words so highest is first.
+		for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+			words[i], words[j] = words[j], words[i]
+		}
+		c.JSON(200, gin.H{
+			"words": words,
+			"total": globalTweetCount,
+		})
+	})
+
+	api.GET("/words/unique_count", func(c *gin.Context) {
+		q := c.Request.URL.Query()
+		period, periodFound := q["period"]
+		targetCountStr, targetCountFound := q["count"]
+
+		var targetCount int64
+		if targetCountFound {
+			var err error
+			targetCount, err = strconv.ParseInt(targetCountStr[0], 10, 64)
+			if err != nil {
+				c.JSON(400, gin.H{
+					"status":  "error",
+					"code":    400,
+					"message": "targetCount must be an int.",
+				})
+				return
+			}
+		}
+
+		var total int64 = 0
+		if !periodFound || period[0] == "focus" {
+			if targetCountFound {
+				globalDiff.Walk(func(word string, count int) {
+					if count == int(targetCount) {
+						total++
+					}
+				})
+			} else {
+				globalDiff.Walk(func(word string, count int) {
+					total++
+				})
+			}
+		} else if period[0] == "long" {
+			if targetCountFound {
+				longGlobalDiff.Walk(func(word string, count int) {
+					if int64(count) == targetCount {
+						total++
+					}
+				})
+			} else {
+				longGlobalDiff.Walk(func(word string, count int) {
+					total++
+				})
+			}
+		} else {
+			c.JSON(400, gin.H{
+				"status":  "error",
+				"code":    400,
+				"message": "Period parameter must be either 'focus' or 'long'.",
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"count": total,
+		})
+	})
+
+	/**
+	 * Returns the count for the given [:word].
+	 * period = [ focus | long ]
+	 * For the [long] period, we use longGlobalDiff. For [focus] we use globalDiff.
+	 */
+	api.GET("/word/:word", func(c *gin.Context) {
+		q := c.Request.URL.Query()
+		period, periodFound := q["period"]
+
+		var count interface{}
+		if !periodFound || period[0] == "focus" {
+			count = globalDiff.Get(c.Param("word"))
+		} else if period[0] == "long" {
+			count = longGlobalDiff.Get(c.Param("word"))
+		} else {
+			c.JSON(400, gin.H{
+				"status":  "error",
+				"code":    400,
+				"message": "Period parameter must be either 'focus' or 'long'.",
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"word":  c.Param("word"),
+			"count": count,
+		})
+	})
+
+	/*
+	 * Produces a protobuf serialized snapshot of the current globalDiff or longGlobalDiff.
+	 * period = [ focus | long ]
+	 * the period determines which globalDiff is being used for the snapshot.
+	 *
+	 * NOTE: The returned data is binary.
+	 */
+	api.GET("/snapshot", func(c *gin.Context) {
+		q := c.Request.URL.Query()
+		period, periodFound := q["period"]
+
+		if !periodFound || period[0] == "focus" {
+			buffer := globalDiff.Serialize()
+			c.Data(200, "application", buffer.Bytes())
+		} else if period[0] == "long" {
+			buffer := longGlobalDiff.Serialize()
+			c.Data(200, "application", buffer.Bytes())
+		} else {
+			c.JSON(400, gin.H{
+				"status":  "error",
+				"code":    400,
+				"message": "Period parameter must be either 'focus' or 'long'.",
+			})
+		}
+	})
+
+	api.GET("/chunks/last", func(c *gin.Context) {
+		diff, ok := wordDiffQueue.Last().(*lib.WordDiff)
+		if ok {
+            buffer := diff.Serialize()
+			c.Data(200, "application", buffer.Bytes())
+		} else {
+			c.JSON(500, gin.H{
+				"status":  "error",
+				"code":    500,
+				"message": "Encountered error reading the latest chunk.",
+			})
+		}
+	})
+
+    api.GET("/chunks/stream", func (c *gin.Context) {
+        count := 0
+        c.Stream(func (w io.Writer) bool {
+            for i := 0; i < 5; i++{
+                log.Println("Writing...")
+                c.SSEvent("message", "test" + string(count))
+                count++
+                time.Sleep(1 * time.Second)
+            }
+            return false
+        })
+    })
+
+	r.GET("/", func(c *gin.Context) {
+		c.File("build/index.html")
+	})
+
+	go tweetsWorker()
+	r.Run("0.0.0.0:8080")
+}
---
+++ twitterWorker.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/CalderWhite/top-tweets/lib"
+)
+
+// mtrie --> Mutable trie
+// strie --> Static trie
+
+/**
+ * Idea: Remove words that are present in larger moving averages to see what is currently trending.
+ * For example, take the 3 day moving average of words and blacklist them all.
+ * This should catch things like the laughing emoji, stopwords, etc. This way we don't have to hardcode
+ * blacklisted words.
+ *
+ * Then, what is left are the words that are not used often.
+ *
+ * This will also help with supporting all languages. I need stopwords for every single language on twitter
+ * without this method.
+ */
+
+// the amount of tweets required to trigger a push to the wordDiffQueue
+const AGG_SIZE int = 300
+
+// the number of AGG_SIZE tweet blocks that will be considered at one time. Once exceeded, we will start deleteing blocks
+/**
+ * (100) * (100) -- last 4 minutes
+ * (300) * (100) -- last 12 minutes
+ * (300) * (300) -- last 36 minutes
+ *
+ */
+const FOCUS_PERIOD int = 300
+
+// the number of tweets between WordDiff.Compress() calls
+const COMPRESS_PERIOD int = 5000
+
+type StreamDataSchema struct {
+	Data struct {
+		Text      string    `json:"text"`
+		ID        string    `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		AuthorID  string    `json:"author_id"`
+	} `json:"data"`
+}
+
+type WordPair struct {
+	Word  string `json:"word"`
+	Count int    `json:"count"`
+}
+
+var wordDiffQueue *lib.CircularQueue = lib.NewCircularQueue(FOCUS_PERIOD)
+var globalDiff *lib.WordDiff = lib.NewWordDiff()
+var longGlobalDiff *lib.WordDiff = lib.NewWordDiff()
+var globalTweetCount int64
+
+func streamTweets(tweets chan<- StreamDataSchema) {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "https://api.twitter.com/2/tweets/sample/stream", nil)
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("TWITTER_BEARER"))
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Println("Error performing request to twitter stream:", err)
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		body, _ := ioutil.ReadAll(resp.Body)
+		log.Fatal("Did not get 200 OK response from twitter API.", string(body))
+		return
+	}
+
+	defer resp.Body.Close()
+	reader := bufio.NewReader(resp.Body)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			log.Println("Got error while reading bytes:", err)
+			// try to read again. Usually it is because the twitter API had nothing to give.
+			if err == io.EOF {
+				break
+			} else {
+				continue
+			}
+		}
+
+		data := StreamDataSchema{}
+		if err := json.Unmarshal(line, &data); err != nil {
+			log.Println("failed to unmarshal bytes:", err)
+			log.Println(string(line))
+			// try to read again. Usually it is because the twitter API had nothing to give.
+			continue
+		}
+
+		tweets <- data
+	}
+}
+
+// this may include removing the @ symbol in the future, among other things.
+func sanatizeWord(word string) string {
+	return strings.ToLower(word)
+}
+
+func isValidWord(word string) bool {
+	// primitive filter to get rid of uninteresting words.
+	// more sophisticated algorithm idea at the top.
+	if len(word) < 3 {
+		return false
+	}
+
+	return true
+}
+
+func processTweets(tweets <-chan StreamDataSchema) {
+	urlRule := regexp.MustCompile(`((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)`)
+	delimRule := regexp.MustCompile(` |"|\.|\,|\!|\?|\:|、|\n`)
+
+	diff := lib.NewWordDiff()
+	tweetCount := 0
+	for tweet := range tweets {
+		globalTweetCount++
+		tweetCount++
+		tweetCount %= AGG_SIZE
+		// this is inefficient. If our process is slowing down, make this is a custom parser.
+		sanatizedText := urlRule.ReplaceAllString(tweet.Data.Text, "")
+		tokens := delimRule.Split(sanatizedText, -1)
+		for _, token := range tokens {
+			word := sanatizeWord(token)
+			validWord := isValidWord(word)
+			if validWord {
+				globalDiff.IncWord(word)
+				longGlobalDiff.IncWord(word)
+				diff.IncWord(word)
+			}
+		}
+
+		if tweetCount == 0 {
+			if wordDiffQueue.IsFull() {
+				oldestDiff, ok := wordDiffQueue.Dequeue().(*lib.WordDiff)
+				if !ok {
+					log.Panic("Could not convert dequeued object to WordDiff.")
+				}
+				globalDiff.Sub(oldestDiff)
+			}
+
+			wordDiffQueue.Enqueue(diff)
+
+			diff = lib.NewWordDiff()
+		}
+	}
+}
+
+func tweetsWorker() {
+	tweets := make(chan StreamDataSchema)
+	go processTweets(tweets)
+
+	// As per the twitter API documentation, the stream can die at times.
+	// So, we must restart the stream when it breaks.
+	for {
+		streamTweets(tweets)
+	}
+}
+
+func getTop(topAmount int) []WordPair {
+	top := make([]WordPair, topAmount)
+	if globalTweetCount/int64((FOCUS_PERIOD*AGG_SIZE)) == 0 {
+		return make([]WordPair, 0)
+	}
+
+	foundNonZero := false
+
+	globalDiff.Lock()
+	longGlobalDiff.Lock()
+	defer globalDiff.Unlock()
+	defer longGlobalDiff.Unlock()
+	globalDiff.WalkUnlocked(func(word string, count int) {
+		longCount := int64(longGlobalDiff.GetUnlocked(word))
+		if longCount == 0 {
+			return
+		}
+
+		// normalize the count. So, if the count is less than the average usage of the the word over LONG_PERIOD, then it will me negative.
+		// In theory, this method should return higher counts for words that are being used more than average.
+		// (LONG_PERIOD / FOCUS_PERIOD) = the factor FOCUS_PERIOD is multiplied by for the LONG_PERIOD
+		// then we divide the long count by that factor to make it the average usage over the past LONG_PERIOD
+		count -= int(longCount / (globalTweetCount / int64(FOCUS_PERIOD*AGG_SIZE)))
+
+		if count > 0 && count > top[0].Count {
+			foundNonZero = true
+			for i := 0; i < len(top); i++ {
+				if count <= top[i].Count {
+					// subtract one since the previous index is the one we are greater than
+					i -= 1
+
+					// shift all those less than <count> back 1
+					copy(top[:i], top[1:i+1])
+					// overwrite the current element
+					top[i] = WordPair{Word: word, Count: count}
+					break
+				} else if i == len(top)-1 {
+					// shift all those less than <count> back 1
+					copy(top[:i], top[1:i+1])
+					// overwrite the current element
+					top[i] = WordPair{Word: word, Count: count}
+					break
+				}
+			}
+		}
+	})
+
+	if foundNonZero {
+		// this usually doesn't happen because there are so many words with 1, but in the odd event that there isn't
+		// we don't want to return zeros because they mess up the UI. Lol.
+		for i, v := range top {
+			if v.Count == 0 {
+				return top[i+1:]
+			}
+		}
+		return top
+	} else {
+		return make([]WordPair, 0)
+	}
+}
